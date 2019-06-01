d8a3b0275894acfc2939afc8b713ef1ce34c8e70
akilakasun1996 <47819516+akilakasun1996@users.noreply.github.com>
2019-06-01 12:06:38 +0530

    Add files via upload

---
+++ Himantha's part/SellNBuy Project/sellnbuy/src/main/java/com/onlineshoppingsystem/sellnbuy/Payment.java
@@ -0,0 +1,74 @@
+package com.onlineshoppingsystem.sellnbuy;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Payment {
+	
+	private int id;
+	private String name;
+	private String country;
+	private String contactNo;
+	private String date;
+	private String creditCardType;
+	private int creditCardNo;
+	private String securityCode;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getCreditCardType() {
+		return creditCardType;
+	}
+	public void setCreditCardType(String creditCardType) {
+		this.creditCardType = creditCardType;
+	}
+	public int getCreditCardNo() {
+		return creditCardNo;
+	}
+	public void setCreditCardNo(int creditCardNo) {
+		this.creditCardNo = creditCardNo;
+	}
+	public String getSecurityCode() {
+		return securityCode;
+	}
+	public void setSecurityCode(String securityCode) {
+		this.securityCode = securityCode;
+	}
+	
+	@Override
+	public String toString() {
+		return "Payment [id=" + id + ", name=" + name + ", country=" + country + ", contactNo=" + contactNo + ", date="
+				+ date + ", creditCardType=" + creditCardType + ", creditCardNo=" + creditCardNo + ", securityCode="
+				+ securityCode + "]";
+	}
+
+
+}
---
+++ Himantha's part/SellNBuy Project/sellnbuy/src/main/java/com/onlineshoppingsystem/sellnbuy/PaymentResource.java
@@ -0,0 +1,99 @@
+package com.onlineshoppingsystem.sellnbuy;
+
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("payments")
+public class PaymentResource {
+	
+	PaymentRepository repo = new PaymentRepository();  
+
+@GET   
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+
+public List<Payment> getPayments() {
+		
+	    System.out.println("getPayment called...");
+		
+	    return repo.getPayments();
+}
+
+
+@GET  
+@Path("payment/{id}")
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+
+public Payment getPayment(@PathParam("id") int id)
+{
+	return repo.getPayment(id);
+	
+}
+
+	@POST
+	@Path("payment") 
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public String createPayment(Payment p1)
+	{
+		Payment p = repo.getPayment(p1.getId());
+		
+	if(p.getId()==0) {
+		System.out.println(p1);
+		repo.create(p1);
+		return "Successfully added the payment!";
+	}
+	else {
+		
+       return "Payment have already done!"; 
+	}
+	
+	}
+
+	@PUT
+	@Path("payment") 
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public String updatePayment(Payment p1)
+	{
+		Payment p = repo.getPayment(p1.getId());
+		
+		if(p.getId()==0) {
+			System.out.println(p1);
+		
+			return "Payment does not exist!";
+		}
+		else {
+			
+			repo.update(p1);
+			return "Successfully updated!"; 
+		}
+}
+	@DELETE
+	@Path("payment/{id}") 
+	public String deletePayment(@PathParam("id") int id) {
+		
+		Payment p = repo.getPayment(id);
+		
+		if(p.getId()==0) {
+			
+		
+			return "Payment does not exist!";
+		}
+		else {
+			
+			repo.delete(id);
+			return "Successfully deleted!"; 
+		}
+		
+	}
+}
